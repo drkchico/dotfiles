a9e7819103f34d6ac51eb06a6aaf17555323d802
Jeff Pacheco <jeff.pacheco731@gmail.com>
2024-05-03 09:11:52 -0400

    update zsh

---
+++ roles/zsh/files/aliases.zsh
@@ -0,0 +1,33 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+alias ll="ls -la"
+
+#alias code="open -a 'Visual Studio Code'"
+
+alias prj="cd $HOME/Documents/git_sites/"
+alias ado="cd $HOME/Documents/git_sites/ado"
+alias gh="cd $HOME/Documents/git_sites/github"
+alias gt="cd $HOME/Documents/git_sites/gitea"
+alias bb="cd $HOME/Documents/git_sites/bitbucket"
+
+
+
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
+alias grep='grep --color'
+
+alias prx1="ssh root@prx-prod-01.jchecophotos.com"
+alias prx2="ssh root@prx-prod-02.jchecophotos.com"
+alias prx3="ssh root@prx-prod-03.jchecophotos.com"
+
+# git aliases
+alias ginit="git init ."
+alias ga="git add ."
+alias gc="git commit -m 'Initial Commit'"
+alias gp="git push"
